4044f2eab851744ff71932e55db17b74967077a7
Ben Marsh <6538947+ben-marsh@users.noreply.github.com>
2025-03-28 10:38:42 -0400

    Command line tool to create a mgcb project from a contentproj. Reuses functionality from the MGCB editor.

---
+++ Tools/MonoGame.Content.Builder.Importer/Global.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MonoGame.Tools.Pipeline
+{
+    public static class Global
+    {
+        public static bool Unix
+            => !OperatingSystem.IsWindows();
+    }
+}
